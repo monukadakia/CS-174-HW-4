a89594cd3c1721bcab0a214221c6ad7dcc679898
Kirtan <janmangal108@gmail.com>
2017-04-26 23:25:36 -0700

    final commit

--- src/controllers/ApiController.php
+++ src/controllers/ApiController.php
@@ -8,10 +8,9 @@ require_once __DIR__."/../configs/CreateDB.php";
 #echo __DIR__."/../configs/CreateDB.php";
 $data = $_POST['data'];
 $webSheetName = $_POST['webSheetName'];
-echo "data is $data";
 
 updateSheet($data, $webSheetName);
-header("Location:/../../index.php?c=api&m=editLayout&webSheetName=$webSheetName");
+//header("Location:__DIR__/../../index.php?c=api&m=editLayout&webSheetName=$webSheetName");
 
 function updateSheet($data, $webSheetName){
 	$db = new Configs\CreateDB();
